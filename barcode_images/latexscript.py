8d9dd537370988d2fec9ec62c0d7ad81c070a40c
Evan Klei <elk7926@rit.edu>
2014-06-27 15:23:30 -0400

    Replaced all qrcodes with new urls

--- barcode_images/latexscript.py
+++ barcode_images/latexscript.py
@@ -2,6 +2,12 @@
 import dircache
 #specify the source path of the images
 __file__="."
+
+add = file("cleanAddress.txt")
+shortToLong = {}
+for line in add:
+    line = line.split()
+    shortToLong[line[1]] = line[0]
 list = dircache.listdir(__file__)
 i = 0
 check = len(list[0])
@@ -9,16 +15,20 @@ temp = []
 count = len(list)
 f1=open("print.tex", "w+")
 suffix = ".png";
+omit = ['text-top.png', 'text-bottom.png']
 
 print >> f1 , "\\documentclass[letterpaper]{article} \n \\usepackage[pdftex]{graphicx} \n \\pagenumbering{gobble} \n \\begin{document} "
 
 
 while count != 0:
-      if(list[i].endswith(suffix)):
-        pre="\\topskip0pt \n \\vspace*{\\fill} \n \n  \\centerline{\\includegraphics[scale=1,width=6in]{text-top.png}} \n \n \\vspace{0.5in} \n \n \\begingroup \n \\centerline{\\includegraphics[scale=1,width=5in,height=5in]{";
-        middle=list[i];
+      if(list[i].endswith(suffix) and not list[i] in omit):
+        pre="\\topskip0pt \n \\vspace*{\\fill} \n \n  \\centerline{\\includegraphics[scale=1,width=6in]{text-top.png}} \n \n \\vspace{0.5in} \n \n \\begingroup \n \\centerline{\\includegraphics[scale=1,width=5.5in,height=5.5in]{";
+	middle=list[i];
         postimg= "}} \n \\endgroup \n \\vspace*{\\fill} \n\n \\hfill{\\small "
-        imgname=list[i][:-4]
+        try:
+	    imgname=shortToLong[list[i][:-4]]
+	except:
+	    imgname=list[i][:-4]
         post = "} \n\n  \\vspace{0.7in} \n \n \\centerline{\\includegraphics[scale=1,width=3in]{text-bottom.png}} \n \n \\pagebreak \n"
         final=''.join([pre, middle, postimg, imgname, post])
         print >> f1, final ,
